3f47b4a94408aa172eae1c26fa8fe969c556daa5
Chinonso Obidike <chinog15@ymail.com>
2019-01-07 23:03:19 -0500

    Update ViewController.swift

--- BarChart/ViewController.swift
+++ BarChart/ViewController.swift
@@ -19,13 +19,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var inputTextField: UITextField!
     
-  
+  //declare global variables
     var i = 1
     var points = 0
     var allScores = [Int]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Introduction.
         instructionLabel.text = "This app gives you a bar chart for your\nscore points"
         reportView.text = "How many scores points do you want to enter?"
         inputTextField.placeholder = "Enter number of scores"
@@ -34,7 +34,7 @@ class ViewController: UIViewController {
     @IBAction func enterButton(_ sender: UIButton) {
         instructionLabel.text = ""
         sender.tag += 1
-        if sender.tag < 2 || points > 10{
+        if sender.tag < 2 || points > 10{ //confirm that number of points entred is not more than 10 or less than 1
             inputTextField.placeholder = "Enter number of scores"
             let point = getNumberOfPoints()
             if point > 10 || points < 1{
@@ -42,12 +42,12 @@ class ViewController: UIViewController {
                 instructionLabel.text = "⚠️ No of scores must not exceed 10"
                 sender.tag == 0
             }
-        } else {
+        } else {                            //get scores for each point
             inputTextField.placeholder = "Enter scores for \(String(i + 1))"
             instructionLabel.textColor = UIColor(red: 0.734, green: 0.307, blue: 0.251, alpha: 1)
             instructionLabel.text = "Enter scores for \(String(i + 1))"
             getScorePoints()
-            if i > points {
+            if i > points {            //ensure the number of scores entered does not exceed number of points
                 sender.isUserInteractionEnabled = false
                 inputTextField.resignFirstResponder();
                 displayBarChart(storeSales: self.allScores)
@@ -55,17 +55,20 @@ class ViewController: UIViewController {
         }
     }
     
-    
+    //function that gets number of points 
     func getNumberOfPoints() -> Int {
         points = Int(inputTextField.text!)!
          reportView.text = ""
         return points
     }
+    
+    //function that gets the score points
     func getScorePoints() {
         inputTextField.placeholder = "Enter scores for \(String(i + 1))"
         var amount: Int = 0
         amount = Int(inputTextField.text!)!
         
+        //`ensure score entered is between 100 and 3000
         if amount < 100 {
             instructionLabel.textColor = .red
             instructionLabel.text = "⚠️ Score points cannot be less than 100"
@@ -85,6 +88,7 @@ class ViewController: UIViewController {
         }
     }
     
+    //function takes the array of points entered and displays bar chart per 100
     func displayBarChart(storeSales: [Int]){
         var count1: Int = 1
         var count2: Int = 1
